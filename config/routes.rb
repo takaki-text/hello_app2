c13e255b61806396ce08ee585d505d9a76006f65
@takaki-text <nagaishitakaki@icloud.com>
2020-04-09 19:25:42 +0900

    プロフィール表示エラー状態　ユーザー情報登録機能作成中

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
  
   
  
-  
+  resources :users, only: [:show]
+  get 'users/index'
+  get 'users/show'
   get"top/index" => "top#index"
   get 'home/index'
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'}
 end
