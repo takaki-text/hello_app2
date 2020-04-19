d11942ec2fb105e171599b5a19a62ca93bc30201
@takaki-text <nagaishitakaki@icloud.com>
2020-04-19 16:29:10 +0900

    viewsに名前表示できない

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
- 
-  
- 
-  resources :users, only: [:show]
   get 'users/index'
   get"top/index" => "top#index"
   get 'home/index'
   devise_for :users, controllers: {
     registrations: 'users/registrations'}
+    resources :users, only: [:show]
 end
