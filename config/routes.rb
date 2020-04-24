6a6bc08903135fef7b2d36e19355f13ef40c5dd3
@takaki-text <nagaishitakaki@icloud.com>
2020-04-24 22:12:37 +0900

    edit routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
   root "top#index"
-  
+  get "courses" => "courses#index"
+  post "courses" => "courses#create"
+  get "new_course" => "courses#new"
+  get "course/:id/edit" => "courses#edit"
+  get "course/:id" => "courses#show"
+  post "courses/:id/update" => "courses#update"
+  put "courses/:id" => "courses#update"
+  delete "courses/:id" => "courses#destroy"
 
   
   devise_for :users, controllers: {
     registrations: 'users/registrations'}
-    resources :courses
+    resources :courses, only:[:show, :index]
+
     resources :users
    
 end
