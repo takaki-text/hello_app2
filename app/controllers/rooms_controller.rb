e7378537535fdc84a1d85e2d32f1e8f65c81fefc
@takaki-text <nagaishitakaki@icloud.com>
2020-05-14 19:10:42 +0900

    修正完了

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,9 +10,7 @@ class RoomsController < ApplicationController
   def show
     @room = Room.find(params[:id])
     if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
-      @messages = @room.messages
       @message = Message.new
-      @entries = @room.entries
     else
       redirect_back(fallback_location: root_path)
     end
