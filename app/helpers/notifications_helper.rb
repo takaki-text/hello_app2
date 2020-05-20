9971898dea305d45a8e077905a1b8ced7e4bf341
@takaki-text <nagaishitakaki@icloud.com>
2020-05-21 01:01:15 +0900

    メッセージ通知機能完成 errer表示あり

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -8,7 +8,7 @@ module NotificationsHelper
             tag.a(notification.visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"があなたをfollowしました"
           when "message" then
               @message = Message.find_by(id: @visitor_message)
-              tag.a(@visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"から"+tag.a('message', href:"/rooms/#{@visitor.room.id}", style:"font-weight: bold;")+"messageが来ました"
+              tag.a(@visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"から"+tag.a('message',  style:"font-weight: bold;")+"messageが来ました"
         end
     end
     
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ApplicationRecord
   has_many :entries, dependent: :destroy
   def create_notification_message!(current_user, message_id)
     # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
-    temp_ids = Message.select(:user_id).where(room_id: id).where.not(user_id: current_user.id).distinct
+    temp_ids = Entry.select(:user_id).where(room_id: id).where.not(user_id: current_user.id).distinct
     temp_ids.each do |temp_id|
       save_notification_message!(current_user, message_id, temp_id['user_id'])
     end
@@ -13,7 +13,6 @@ class Room < ApplicationRecord
   def save_notification_message!(current_user, message_id, visited_id)
     # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
     notification = current_user.active_notifications.new(
-      room_id: id,
       message_id: message_id,
       visited_id: visited_id,
       action: 'message'
