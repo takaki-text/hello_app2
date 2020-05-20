4e83aae506d72c87ca6840167d9c0d789b16e191
@takaki-text <nagaishitakaki@icloud.com>
2020-05-20 18:16:21 +0900

    メッセージ通知機能しない

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,16 +1,14 @@
 module NotificationsHelper
     def notification_form(notification)
-        @visiter = notification.visiter
+        @visitor = notification.visitor
         @message = nil
-        your_item = link_to 'あなたの投稿', room_path(notification), style:"font-weight: bold;"
-        @visiter_message = notification.message_id
-        #notification.actionがfollowかlikeかcommentか
+        @visitor_message = notification.message_id
         case notification.action
           when "follow" then
-            tag.a(notification.visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"があなたをfollowしました"
+            tag.a(notification.visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"があなたをfollowしました"
           when "message" then
-              @comment = Message.find_by(id: @visiter_message)
-              tag.a(@visiter.name, href:users_user_path(@visiter), style:"font-weight: bold;")+"から"+tag.a('', href:users_item_path(notification.item_id), style:"font-weight: bold;")+"messageが来ました"
+              @message = Message.find_by(id: @visitor_message)
+              tag.a(@visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"から"+tag.a('message', href:"/rooms/#{@visitor.room.id}", style:"font-weight: bold;")+"messageが来ました"
         end
     end
     
