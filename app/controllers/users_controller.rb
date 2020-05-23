3b86f5bba7bec8f48c51b65f3c54444da911e89a
@takaki-text <nagaishitakaki@icloud.com>
2020-05-23 22:37:05 +0900

    未通知機能でない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
-  
+  before_action :authenticate_user!
   def show
     @user = User.find(params[:id])
     @courses = @user.courses
     @demands = @user.demands
-  end
-  def index
-    @user = User.all
-    @courses = @user.courses
+
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
+  
+
   end
   def show2
     @user = User.find(params[:id])
