ce369e6b9d1298c2570328028c1c25005a679f94
@takaki-text <nagaishitakaki@icloud.com>
2020-05-23 12:00:38 +0900

    rooms errer

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,13 +3,24 @@ module NotificationsHelper
         @visitor = notification.visitor
         @message = nil
         @visitor_message = notification.message_id
+        @currentUserEntry=Entry.where(user_id: current_user.id)
+        @userEntry=Entry.where(user_id: @visitor.id)
+          @currentUserEntry.each do |cu|
+            @userEntry.each do |u|
+              if cu.room_id == u.room_id then
+                @roomId = cu.room_id
+              end
+            end
+          end
+
         case notification.action
           when "follow" then
             tag.a(notification.visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"があなたをfollowしました"
           when "message" then
               @message = Message.find_by(id: @visitor_message)
-              tag.a(@visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"から"+tag.a('message',  style:"font-weight: bold;")+"messageが来ました"
+              tag.a(@visitor.name, href:show2_user_path(@visitor), style:"font-weight: bold;")+"から"+tag.a('message', href:rooms/(@roomId), style:"font-weight: bold;")+"が来ました"
         end
     end
     
 end
+
