c445c7455b6296b271544f51af3262d5eb60abb5
@takaki-text <nagaishitakaki@icloud.com>
2020-05-25 10:39:58 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -35,7 +35,7 @@ class User < ApplicationRecord
           self.followings.include?(other_user)
          end
          def create_notification_follow!(current_user)
-          temp = Notification.where(["visitor_id = ? and visited_id = ? and action = ? ",current_user.id, id, 'follow'])
+          temp = Notification.where(visitor_id: current_user.id).where(visited_id: id).where(action: 'follow')
           if temp.blank?
             notification = current_user.active_notifications.new(
               visited_id: id,
