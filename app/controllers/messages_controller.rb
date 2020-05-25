ddbac34d24be2a3c021172c76b24f968fc05649c
@takaki-text <nagaishitakaki@icloud.com>
2020-05-25 22:23:09 +0900

    修正完了

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,7 @@ class MessagesController < ApplicationController
       if Entry.where(:user_id => current_user.id, :room_id => params[:message][:room_id]).present?
         @message = Message.create(params.require(:message).permit(:user_id, :content, :room_id).merge(:user_id => current_user.id))
         @room = @message.room
-        @room.create_notification_message!(current_user, @message.id)
+        @room.create_notification_message(current_user, @message.id)
         redirect_to "/rooms/#{@message.room_id}"
       else
         redirect_back(fallback_location: root_path)
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,16 +1,16 @@
 class Room < ApplicationRecord
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
-  def create_notification_message!(current_user, message_id)
+  def create_notification_message(current_user, message_id)
     # 自分以外にコメントしている人をすべて取得し、全員に通知を送る
     temp_ids = Entry.select(:user_id).where(room_id: id).where.not(user_id: current_user.id).distinct
     temp_ids.each do |temp_id|
-      save_notification_message!(current_user, message_id, temp_id['user_id'])
+      save_notification_message(current_user, message_id, temp_id['user_id'])
     end
 
   end
 
-  def save_notification_message!(current_user, message_id, visited_id)
+  def save_notification_message(current_user, message_id, visited_id)
     # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
     notification = current_user.active_notifications.new(
       room_id: id,
